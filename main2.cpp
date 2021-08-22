a7e41c9326a4b3d55ad811f3019cbe4cf742bfe0
Rodion <82750919+icerazor1989@users.noreply.github.com>
2021-08-22 15:19:32 +0300

    Update and rename main (5).cpp to main2.cpp

--- main2.cpp
+++ main2.cpp
@@ -3,7 +3,6 @@
 
 using namespace std;
 
-
 class RGBA
 {
     uint8_t m_red;
@@ -13,40 +12,28 @@ class RGBA
  
 public:
     RGBA () {
-
-
         m_red = 0;
         m_green = 0;
         m_blue = 0;
         m_alpha = 255;
-
     }
+    
     void setRGBA(uint8_t r, uint8_t g, uint8_t b)
     {
-
         m_red = r;
         m_green = g;;
         m_blue = b;
-        
-
     }
-
 
       void print()
     {
-
       cout <<"r= "<< static_cast<int> (m_red) <<", g= " << static_cast<int> (m_green)<<
            ", b= " << static_cast<int> (m_blue) <<", a= " << static_cast<int> (m_alpha);
-
     }
-
-
 };
 
-
 int main()
-{
-    
+{    
     RGBA color;
     color.setRGBA(0, 135, 135);
     color.print();
